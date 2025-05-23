49aad3cf7b864904ea188fb81e6a8931e77a18a4
SmartLamScott <134100828+SmartLamScott@users.noreply.github.com>
2025-05-23 11:02:52 -0500

    Revert "Release-v0.1.3"

--- src/object_filtering/natural_language/__init__.py
+++
@@ -1,7 +0,0 @@
-# (c) 2025 Scott Ratchford
-# This file is licensed under the MIT License. See LICENSE.txt for details.
-
-from .natural_language import (
-    explain_expression,
-    explain_filter,
-)
